a71a514a0243e782072b2279b1d15be99b000bc6
Varun Srivastava <varun091137ece@gmail.com>
2026-08-24 11:58:33 -0700

    adding latest configs

---
+++ assets/config/a4ec.cfg.diff.lua
@@ -0,0 +1,769 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2003cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_RZ",
+				},
+			},
+			["name"] = "Rudder",
+		},
+		["a2012cdnil"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_RY",
+				},
+			},
+			["name"] = "Zoom View",
+		},
+		["a3204cdnil"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_RZ",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "Wheel Brake Differential (Single-Axis)",
+		},
+	},
+	["keyDiffs"] = {
+		["d10007pnilu10008cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Wheel Brake - ON else OFF",
+		},
+		["d10011pnilu10010cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "Flap Handle - UP else STOP",
+		},
+		["d10012pnilu10010cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "Flap Handle - DOWN else STOP",
+		},
+		["d10013pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+					["reformers"] = {
+						[1] = "F2",
+						[2] = "F3",
+					},
+				},
+			},
+			["name"] = "Spoiler Arm Switch - ARM/OFF",
+		},
+		["d10016pnilu10017cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Gun-Rocket Trigger",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+		},
+		["d10018pnilu10019cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "Bomb Release Button",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+		},
+		["d10020pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN20",
+				},
+			},
+			["name"] = "Master Armament Switch - ON/OFF",
+		},
+		["d10021pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN19",
+				},
+			},
+			["name"] = "Guns Charging Switch - READY/SAFE",
+		},
+		["d10022pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "Station 1 Selector - READY/OFF",
+		},
+		["d10023pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "Station 2 Selector - READY/OFF",
+		},
+		["d10024pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "Station 3 Selector - READY/OFF",
+		},
+		["d10025pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "Station 4 Selector - READY/OFF",
+		},
+		["d10026pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "Station 5 Selector - READY/OFF",
+		},
+		["d10027pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN18",
+				},
+			},
+			["name"] = "Weapon Function Selector Switch - CCW",
+		},
+		["d10028pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "Weapon Function Selector Switch - CW",
+		},
+		["d10034pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+					["reformers"] = {
+						[1] = "F2",
+						[2] = "F3",
+					},
+				},
+			},
+			["name"] = "Engine Starter Switch - START",
+		},
+		["d10061pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AN/APN-153 Doppler Navigation Radar Mode Selector - CW",
+		},
+		["d10062pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AN/APN-153 Doppler Navigation Radar Mode Selector - CCW",
+		},
+		["d10063pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AN/ASN-41 Function Selector Switch - CW",
+		},
+		["d10064pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN19",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AN/ASN-41 Function Selector Switch - CCW",
+		},
+		["d10074pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "AN/APG-53A Radar Mode Selector - Rotary CW",
+		},
+		["d10075pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "AN/APG-53A Radar Mode Selector - Rotary CCW",
+		},
+		["d10076pnilunilcdnilvd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+				},
+			},
+			["name"] = "Radar Terrain Clearance - PROFILE/PLAN",
+		},
+		["d10085pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN18",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AN/ARN-52 TACAN Mode Switch - CW",
+		},
+		["d10086pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AN/ARN-52 TACAN Mode Switch - CCW",
+		},
+		["d10101pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "Master Exterior Lights Switch - ON/OFF",
+		},
+		["d10121pnilunilcdnilvd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "APC Power Switch - STBY",
+		},
+		["d10121pnilunilcdnilvd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "APC Power Switch - ENGAGE",
+		},
+		["d10125pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "AFCS Engage Switch - ENGAGE/OFF",
+		},
+		["d10126pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "AFCS Altitude Switch - ALT/OFF",
+		},
+		["d10127pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "AFCS Heading Select Switch - HDG SEL/OFF",
+		},
+		["d10171pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AWRS Quantity Selector Switch - CW/Increase",
+		},
+		["d10172pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AWRS Quantity Selector Switch - CCW/Decrease",
+		},
+		["d10173pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AWRS Mode Selector Switch - CCW",
+		},
+		["d10174pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "AWRS Mode Selector Switch - CW",
+		},
+		["d10234pnilu10236cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+				},
+			},
+			["name"] = "Radar Antenna Tilt - Continuous CW",
+		},
+		["d10235pnilu10236cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+			["name"] = "Radar Antenna Tilt - Continuous CCW",
+		},
+		["d10284pnilu10285cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DR",
+				},
+			},
+			["name"] = "UI Override - Right Shoulder (LCtrl + Mouse 1/Left Click)",
+		},
+		["d10286pnilu10287cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DL",
+				},
+			},
+			["name"] = "UI Override - Left Shoulder (LShift + Mouse 2/Right Click)",
+		},
+		["d10288pnilu10290cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "AFCS Heading Select Pull-to-Set Knob - Continuous Increase",
+		},
+		["d10289pnilu10290cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+				},
+			},
+			["name"] = "AFCS Heading Select Pull-to-Set Knob - Continuous Decrease",
+		},
+		["d10291pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "NVG Night Vision Goggle - Toggle",
+		},
+		["d120pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+					["reformers"] = {
+						[1] = "F2",
+						[2] = "F3",
+					},
+				},
+			},
+			["name"] = "Catapult Hook-Up",
+		},
+		["d147pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "Speedbrake Switch - OPEN",
+		},
+		["d148pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Speedbrake Switch - CLOSE",
+		},
+		["d177pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+					["reformers"] = {
+						[1] = "Pinky",
+					},
+				},
+			},
+			["name"] = "Zoom normal",
+		},
+		["d179pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "Communication menu",
+		},
+		["d238pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+					["reformers"] = {
+						[1] = "F2",
+						[2] = "F3",
+					},
+				},
+			},
+			["name"] = "Pilot Salute",
+		},
+		["d3157pnilunilcd20vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "Throttle Position Lock - Step Down (OFF/IGN/IDLE)",
+		},
+		["d3157pnilunilcd20vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+					["reformers"] = {
+						[1] = "F3",
+					},
+				},
+			},
+			["name"] = "Throttle Position Lock - Step Up (OFF/IGN/IDLE)",
+		},
+		["d363pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "Mouse Clickable Cockpit Mode - ON/OFF",
+		},
+		["d491pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN21",
+				},
+			},
+			["name"] = "Cockpit Camera Move Center",
+		},
+		["d68pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+				},
+			},
+			["name"] = "Landing Gear Handle - UP/DOWN",
+		},
+		["d71pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+					["reformers"] = {
+						[1] = "F2",
+						[2] = "F3",
+					},
+				},
+			},
+			["name"] = "Canopy Lever - OPEN/CLOSE",
+		},
+		["d840pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "NVG Night Vision Goggle Gain - Increase",
+		},
+		["d841pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "NVG Night Vision Goggle Gain - Decrease",
+		},
+		["dnilp10113u10119cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+			["name"] = "Trim Switch - NOSE UP",
+		},
+		["dnilp10114u10119cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+			["name"] = "Trim Switch - NOSE DOWN",
+		},
+		["dnilp10115u10119cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+			["name"] = "Trim Switch - LEFT WING DOWN",
+		},
+		["dnilp10116u10119cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+			["name"] = "Trim Switch - RIGHT WING DOWN",
+		},
+		["dnilp10120u10119cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+				[2] = {
+					["key"] = "JOY_BTN_POV1_UL",
+				},
+			},
+			["name"] = "Trim Reset (Hold)",
+		},
+		["dnilp210u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UR",
+				},
+			},
+		},
+		["dnilp211u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DR",
+				},
+			},
+		},
+		["dnilp212u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DL",
+				},
+			},
+		},
+		["dnilp213u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UL",
+				},
+			},
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+		["dnilp36unilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+					["reformers"] = {
+						[1] = "F2",
+					},
+				},
+			},
+			["name"] = "View Center",
+		},
+		["dnilp39u289cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+					["reformers"] = {
+						[1] = "Pinky",
+					},
+				},
+			},
+			["name"] = "Zoom in slow",
+		},
+		["dnilp488u490cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN22",
+				},
+			},
+			["name"] = "Cockpit Camera Move Forward",
+		},
+	},
+}
+return diff
